2cce2a6ef1a0fee2b769c9abaadfe3ee9ca639b1
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-27 17:04:13 -0400

    Update GameComputer.js

--- src/pages/GameComputer.js
+++ src/pages/GameComputer.js
@@ -122,216 +122,218 @@ const GameComputer = () => {
 
   return (
     <>
-      <div className={styles.gamecomputer}>
-        <div className={styles.content1Topuser}>
-          <div className={styles.textuser1score}>Score: 5</div>
-          <img
-            className={styles.imguser1avatarIcon}
-            alt=""
-            src="/imguser1avatar@2x.png"
-          />
-          <div className={styles.textuser1elo}>(elo)</div>
-          <b className={styles.textuser1}>Username1</b>
-        </div>
-        <div className={styles.content2Chess}>
-          <div
-            className={styles.chessboardcontainer}
-            id="divChessboardContainer"
-          />
-          <div id="chessBoard"></div>
-        </div>
-        <div className={styles.content3Bottomuser}>
-          <div className={styles.textuser2score}>Score: 5</div>
-          <img
-            className={styles.imguser2avatarIcon}
-            alt=""
-            src="/imguser1avatar@2x.png"
-          />
-          <div className={styles.textuser2elo}>(elo)</div>
-          <button
-            className={styles.buttonaddfriend}
-            onClick={onButtonAddFriendClick}
-          >
-            <b className={styles.addfriendbuttontext}>Add Friend</b>
-          </button>
-          <b className={styles.textuser2}>Username2</b>
-        </div>
-        <div className={styles.content4Buttons}>
-          <div className={styles.content4divider} />
-          <button
-            className={styles.buttonconcede}
-            onClick={onButtonConcedeClick}
-          >
-            <b className={styles.concedebuttontext}>Concede</b>
-          </button>
-          <button className={styles.buttonleave} onClick={openPopupLeaveLarge}>
-            <b className={styles.concedebuttontext}>Leave Match</b>
-          </button>
-        </div>
-        <div className={styles.footer}>
-          <button
-            className={styles.footerimggithub}
-            onClick={onFooterImgGitHubClick}
-          />
-          <div className={styles.footertextcopyright}>
-            © 2023 FullStack Bros. All rights reserved.
-          </div>
-          <button
-            className={styles.footerbuttonback}
-            onClick={onFooterButtonBackClick}
-          >
-            <b className={styles.footerbuttontext}>Back to Top</b>
-          </button>
-          <button
-            className={styles.footerimgdiscord}
-            onClick={onFooterImgDiscordClick}
-          />
-        </div>
-        <div
-          className={styles.navigationbarsi}
-          data-scroll-to="navigationBarSIContainer"
-        >
-          <div className={styles.dropdown}>
-            <div className={styles.dropbackground} />
-            <div className={styles.dropselectlogout} />
-            <b className={styles.droplogout} onClick={openPopupLeaveLarge1}>
-              Log Out
-            </b>
-            <div className={styles.dropselectfriends} />
-            <b className={styles.dropfriends} onClick={openPopupLeaveLarge2}>
-              Friends
-            </b>
-            <div className={styles.dropselectprofile} />
-            <b className={styles.dropprofile} onClick={openPopupLeaveLarge3}>
-              Profile
-            </b>
-          </div>
-          <div className={styles.navbar}>
-            <div className={styles.navbarbackground} />
-            <div className={styles.navprofilecorner}>
-              <img
-                className={styles.navprofilepicIcon}
-                alt=""
-                src="/navprofilepic@2x.png"
-                onClick={openPopupLeaveLarge4}
-              />
-              <div className={styles.navelo}>(9001)</div>
-              <b className={styles.navusername} onClick={openPopupLeaveLarge5}>
-                Username
-              </b>
-            </div>
-            <div className={styles.pageselectcontact} />
-            <b className={styles.navcontact} onClick={openPopupLeaveLarge6}>
-              Contact Us
-            </b>
-            <div className={styles.pageselecthow} />
-            <b className={styles.navhow} onClick={openPopupLeaveLarge7}>
-              How to Play
-            </b>
-            <div className={styles.pageselectabout} />
-            <b className={styles.navabout} onClick={openPopupLeaveLarge8}>
-              About
-            </b>
-            <div className={styles.pageselectlogo} />
+      <body onload="createBoard()">
+        <div className={styles.gamecomputer}>
+          <div className={styles.content1Topuser}>
+            <div className={styles.textuser1score}>Score: 5</div>
             <img
-              className={styles.navlogoIcon}
+              className={styles.imguser1avatarIcon}
               alt=""
-              src="/navlogo@2x.png"
-              onClick={openPopupLeaveLarge9}
+              src="/imguser1avatar@2x.png"
+            />
+            <div className={styles.textuser1elo}>(elo)</div>
+            <b className={styles.textuser1}>Username1</b>
+          </div>
+          <div className={styles.content2Chess}>
+            <div
+              className={styles.chessboardcontainer}
+              id="divChessboardContainer"
+            />
+            <div id="chessBoard"></div>
+          </div>
+          <div className={styles.content3Bottomuser}>
+            <div className={styles.textuser2score}>Score: 5</div>
+            <img
+              className={styles.imguser2avatarIcon}
+              alt=""
+              src="/imguser1avatar@2x.png"
+            />
+            <div className={styles.textuser2elo}>(elo)</div>
+            <button
+              className={styles.buttonaddfriend}
+              onClick={onButtonAddFriendClick}
+            >
+              <b className={styles.addfriendbuttontext}>Add Friend</b>
+            </button>
+            <b className={styles.textuser2}>Username2</b>
+          </div>
+          <div className={styles.content4Buttons}>
+            <div className={styles.content4divider} />
+            <button
+              className={styles.buttonconcede}
+              onClick={onButtonConcedeClick}
+            >
+              <b className={styles.concedebuttontext}>Concede</b>
+            </button>
+            <button className={styles.buttonleave} onClick={openPopupLeaveLarge}>
+              <b className={styles.concedebuttontext}>Leave Match</b>
+            </button>
+          </div>
+          <div className={styles.footer}>
+            <button
+              className={styles.footerimggithub}
+              onClick={onFooterImgGitHubClick}
+            />
+            <div className={styles.footertextcopyright}>
+              © 2023 FullStack Bros. All rights reserved.
+            </div>
+            <button
+              className={styles.footerbuttonback}
+              onClick={onFooterButtonBackClick}
+            >
+              <b className={styles.footerbuttontext}>Back to Top</b>
+            </button>
+            <button
+              className={styles.footerimgdiscord}
+              onClick={onFooterImgDiscordClick}
             />
           </div>
+          <div
+            className={styles.navigationbarsi}
+            data-scroll-to="navigationBarSIContainer"
+          >
+            <div className={styles.dropdown}>
+              <div className={styles.dropbackground} />
+              <div className={styles.dropselectlogout} />
+              <b className={styles.droplogout} onClick={openPopupLeaveLarge1}>
+                Log Out
+              </b>
+              <div className={styles.dropselectfriends} />
+              <b className={styles.dropfriends} onClick={openPopupLeaveLarge2}>
+                Friends
+              </b>
+              <div className={styles.dropselectprofile} />
+              <b className={styles.dropprofile} onClick={openPopupLeaveLarge3}>
+                Profile
+              </b>
+            </div>
+            <div className={styles.navbar}>
+              <div className={styles.navbarbackground} />
+              <div className={styles.navprofilecorner}>
+                <img
+                  className={styles.navprofilepicIcon}
+                  alt=""
+                  src="/navprofilepic@2x.png"
+                  onClick={openPopupLeaveLarge4}
+                />
+                <div className={styles.navelo}>(9001)</div>
+                <b className={styles.navusername} onClick={openPopupLeaveLarge5}>
+                  Username
+                </b>
+              </div>
+              <div className={styles.pageselectcontact} />
+              <b className={styles.navcontact} onClick={openPopupLeaveLarge6}>
+                Contact Us
+              </b>
+              <div className={styles.pageselecthow} />
+              <b className={styles.navhow} onClick={openPopupLeaveLarge7}>
+                How to Play
+              </b>
+              <div className={styles.pageselectabout} />
+              <b className={styles.navabout} onClick={openPopupLeaveLarge8}>
+                About
+              </b>
+              <div className={styles.pageselectlogo} />
+              <img
+                className={styles.navlogoIcon}
+                alt=""
+                src="/navlogo@2x.png"
+                onClick={openPopupLeaveLarge9}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      {isPopupLeaveLargeOpen && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge1Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge1}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge1} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge2Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge2}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge2} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge3Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge3}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge3} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge4Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge4}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge4} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge5Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge5}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge5} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge6Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge6}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge6} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge7Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge7}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge7} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge8Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge8}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge8} />
-        </PortalPopup>
-      )}
-      {isPopupLeaveLarge9Open && (
-        <PortalPopup
-          overlayColor="rgba(0, 0, 0, 0.66)"
-          placement="Centered"
-          onOutsideClick={closePopupLeaveLarge9}
-        >
-          <PopupLeaveLarge onClose={closePopupLeaveLarge9} />
-        </PortalPopup>
-      )}
+        {isPopupLeaveLargeOpen && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge1Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge1}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge1} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge2Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge2}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge2} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge3Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge3}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge3} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge4Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge4}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge4} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge5Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge5}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge5} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge6Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge6}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge6} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge7Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge7}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge7} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge8Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge8}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge8} />
+          </PortalPopup>
+        )}
+        {isPopupLeaveLarge9Open && (
+          <PortalPopup
+            overlayColor="rgba(0, 0, 0, 0.66)"
+            placement="Centered"
+            onOutsideClick={closePopupLeaveLarge9}
+          >
+            <PopupLeaveLarge onClose={closePopupLeaveLarge9} />
+          </PortalPopup>
+        )}
+      </body>
     </>
   );
 };
