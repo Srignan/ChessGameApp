8b0b90e25c26c01fc8e791060f5fdc4304fb3ce8
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-27 16:33:14 -0400

    Update GameComputer.js

--- src/pages/GameComputer.js
+++ src/pages/GameComputer.js
@@ -2,9 +2,9 @@ import { useState, useCallback } from "react";
 import PopupLeaveLarge from "../components/PopupLeaveLarge";
 import PortalPopup from "../components/PortalPopup";
 import styles from "./GameComputer.module.css";
-import 'src/pages/pieces';
-import 'src/pages/ui';
-import 'src/pages/chessBoard'; 
+import 'src/pages/pieces.js';
+import 'src/pages/ui.js';
+import 'src/pages/chessBoard.js'; 
 
 const GameComputer = () => {
   const [isPopupLeaveLargeOpen, setPopupLeaveLargeOpen] = useState(false);
