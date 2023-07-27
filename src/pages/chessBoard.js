f2ca23f67d57ae76eb0e5a594d89915aeb2afebb
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-27 16:28:37 -0400

    Update chessBoard.js

--- src/pages/chessBoard.js
+++ src/pages/chessBoard.js
@@ -1,5 +1,5 @@
-import '/src/pages/pieces';
-import '/src/pages/ui';
+import 'src/pages/pieces';
+import 'src/pages/ui';
 
 const chessBoard = document.querySelector("#chessBoard"); 
 const playerDisplay = document.querySelector("#player");
