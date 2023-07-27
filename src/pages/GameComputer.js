c642fa14cb2fd10a4b48da80ae37ca9aa67de22f
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-27 16:12:41 -0400

    Update GameComputer.js

--- src/pages/GameComputer.js
+++ src/pages/GameComputer.js
@@ -2,8 +2,6 @@ import { useState, useCallback } from "react";
 import PopupLeaveLarge from "../components/PopupLeaveLarge";
 import PortalPopup from "../components/PortalPopup";
 import styles from "./GameComputer.module.css";
-import './pieces';
-import './ui';
 import './chessBoard'; 
 
 const GameComputer = () => {
