3416c7fa18ff9de630891bc1f0683a37e3f07eef
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-27 11:31:14 -0400

    Add files via upload

---
+++ src/pages/ui.js
@@ -0,0 +1,5 @@
+//divs containing SVG's of ui elements
+
+const circleNoPiece = '<svg xmlns="http://www.w3.org/2000/svg"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"/></svg>';
+
+const circlePiece = '<svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" xml:space="preserve"><path d="M150,0C67.29,0,0,67.29,0,150s67.29,150,150,150s150-67.29,150-150S232.71,0,150,0z M150,270c-66.169,0-120-53.832-120-120 S83.831,30,150,30s120,53.832,120,120S216.168,270,150,270z"/></svg>';
