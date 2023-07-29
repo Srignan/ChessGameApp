be5dc7c955fbba83af37f53b5a3750d1d1efc55b
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-29 14:59:53 -0400

    Update GameComputer.js

--- src/pages/GameComputer.js
+++ src/pages/GameComputer.js
@@ -141,7 +141,7 @@ useEffect(() => {
         </div>
         <div className={styles.content2Chess}>
           <div className={styles.chessboardcontainer} id="divChessboardContainer">
-            <div id={styles#chessBoard}></div>
+            <div id={styles.chessBoard}></div>
 	    <script src="chessBoard.js"></script>
           </div>
         </div>
