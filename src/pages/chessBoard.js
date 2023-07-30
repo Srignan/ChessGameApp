fc2b085cceeb08e437090ba43ef46c2a7c04da3e
ImLoganLol <135251931+ImLoganLol@users.noreply.github.com>
2023-07-29 23:43:14 -0400

    Update chessBoard.js

--- src/pages/chessBoard.js
+++ src/pages/chessBoard.js
@@ -64,8 +64,8 @@ function createBoard()
 		{
 			let svgDiv = square.children[0];
 			svgDiv.setAttribute("draggable", true);
-			svgDiv.setAttribute("onDragStart", this.dragStart(e));
-			svgDiv.setAttribute("onDrop", this.dragDrop(e));
+			svgDiv.setAttribute("onDragStart", dragStart);
+			svgDiv.setAttribute("onDrop", dragDrop);
 			
 			let svg = square.children[0].children[0];
 			svg.classList.add(styles.svgPiece);
