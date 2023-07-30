e07edad49b955b8911601d46c2b4b78e40b5bd57
Kevin <55450760+kevinbopp@users.noreply.github.com>
2023-07-30 18:52:03 -0400

    Updated all routing for responsive pages

--- src/App.js
+++ src/App.js
@@ -7,33 +7,34 @@ import {
 import Index from "./pages/Index";
 import IndexTablet from "./pages/IndexTablet";
 import IndexMobile from "./pages/IndexMobile";
-import UserSearchComputer from "./pages/UserSearchComputer";
-import LandingComputer from "./pages/LandingComputer";
-import ProfileComputer from "./pages/ProfileComputer";
-import GameComputer from "./pages/GameComputer";
-import GameTablet from "./pages/GameTablet";
-import GameMobile from "./pages/GameMobile";
-import ResetPasswordComputer from "./pages/ResetPasswordComputer";
-import ResetPasswordTablet from "./pages/ResetPasswordTablet";
-import ResetPasswordMobile from "./pages/ResetPasswordMobile";
-import AboutComputer from "./pages/AboutComputer";
-import HowToPlayComputer from "./pages/HowToPlayComputer";
-import ContactUsComputer from "./pages/ContactUsComputer";
+import About from "./pages/About";
 import AboutTablet from "./pages/AboutTablet";
 import AboutMobile from "./pages/AboutMobile";
+import HowToPlay from "./pages/HowToPlay";
 import HowToPlayTablet from "./pages/HowToPlayTablet";
 import HowToPlayMobile from "./pages/HowToPlayMobile";
+import ContactUs from "./pages/ContactUs";
 import ContactUsTablet from "./pages/ContactUsTablet";
 import ContactUsMobile from "./pages/ContactUsMobile";
-import FriendSearchComputer from "./pages/FriendSearchComputer";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordTablet from "./pages/ResetPasswordTablet";
+import ResetPasswordMobile from "./pages/ResetPasswordMobile";
+import Landing from "./pages/Landing";
 import LandingTablet from "./pages/LandingTablet";
 import LandingMobile from "./pages/LandingMobile";
+import Profile from "./pages/Profile";
 import ProfileTablet from "./pages/ProfileTablet";
 import ProfileMobile from "./pages/ProfileMobile";
+import UserSearch from "./pages/UserSearch";
 import UserSearchTablet from "./pages/UserSearchTablet";
 import UserSearchMobile from "./pages/UserSearchMobile";
+import FriendSearch from "./pages/FriendSearch";
 import FriendSearchTablet from "./pages/FriendSearchTablet";
 import FriendSearchMobile from "./pages/FriendSearchMobile";
+import Game from "./pages/Game";
+import GameTablet from "./pages/GameTablet";
+import GameMobile from "./pages/GameMobile";
+
 import { useEffect } from "react";
 
 function App() {
@@ -56,40 +57,8 @@ function App() {
         title = "Chess";
         metaDescription = "";
         break;
-      case "/usersearch":
-        title = "Users | Chess";
-        metaDescription = "";
-        break;
-      case "/gametablet":
-        title = "Game | Chess";
-        metaDescription = "";
-        break;
-      case "/gamemobile":
-        title = "Game | Chess";
-        metaDescription = "";
-        break;
-      case "/landing":
-        title = "Home | Chess";
-        metaDescription = "";
-        break;
-      case "/profile":
-        title = "Profile | Chess";
-        metaDescription = "";
-        break;
-      case "/game":
-        title = "Game | Chess";
-        metaDescription = "";
-        break;
-      case "/resetpasswordcomputer":
-        title = "Reset Password | Chess";
-        metaDescription = "";
-        break;
-      case "/resetpasswordtablet":
-        title = "Reset Password | Chess";
-        metaDescription = "";
-        break;
-      case "/resetpasswordmobile":
-        title = "Reset Password | Chess";
+      case "/index":
+        title = "Chess";
         metaDescription = "";
         break;
       case "/about":
@@ -104,72 +73,28 @@ function App() {
         title = "Contact Us | Chess";
         metaDescription = "";
         break;
-      case "/indextablet":
-        title = "Chess";
+      case "/resetpassword":
+        title = "Reset Password | Chess";
         metaDescription = "";
         break;
-      case "/indexmobile":
-        title = "Chess";
+      case "/landing":
+        title = "Home | Chess";
         metaDescription = "";
         break;
-      case "/abouttablet":
-        title = "About | Chess";
+      case "/profile":
+        title = "Profile | Chess";
         metaDescription = "";
         break;
-      case "/aboutmobile":
-        title = "About | Chess";
-        metaDescription = "";
-        break;
-      case "/howtoplaytablet":
-        title = "How to Play | Chess";
-        metaDescription = "";
-        break;
-      case "/howtoplaymobile":
-        title = "How to Play | Chess";
-        metaDescription = "";
-        break;
-      case "/contactustablet":
-        title = "Contact Us | Chess";
-        metaDescription = "";
-        break;
-      case "/contactusmobile":
-        title = "Contact Us | Chess";
+      case "/usersearch":
+        title = "Users | Chess";
         metaDescription = "";
         break;
       case "/friendsearch":
         title = "Friends | Chess";
         metaDescription = "";
         break;
-      case "/landingtablet":
-        title = "Home | Chess";
-        metaDescription = "";
-        break;
-      case "/landingmobile":
-        title = "Home | Chess";
-        metaDescription = "";
-        break;
-      case "/profiletablet":
-        title = "Profile | Chess";
-        metaDescription = "";
-        break;
-      case "/profilemobile":
-        title = "Profile | Chess";
-        metaDescription = "";
-        break;
-      case "/usersearchtablet":
-        title = "Users | Chess";
-        metaDescription = "";
-        break;
-      case "/usersearchmobile":
-        title = "Users | Chess";
-        metaDescription = "";
-        break;
-      case "/friendsearchtablet":
-        title = "Friends | Chess";
-        metaDescription = "";
-        break;
-      case "/friendsearchmobile":
-        title = "Friends | Chess";
+      case "/game":
+        title = "Game | Chess";
         metaDescription = "";
         break;
     }
@@ -191,38 +116,16 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/usersearch" element={<UserSearchComputer />} />
-      <Route path="/gametablet" element={<GameTablet />} />
-      <Route path="/gamemobile" element={<GameMobile />} />
-      <Route path="/landing" element={<LandingComputer />} />
-      <Route path="/profile" element={<ProfileComputer />} />
-      <Route path="/game" element={<GameComputer />} />
-      <Route
-        path="/resetpasswordcomputer"
-        element={<ResetPasswordComputer />}
-      />
-      <Route path="/resetpasswordtablet" element={<ResetPasswordTablet />} />
-      <Route path="/resetpasswordmobile" element={<ResetPasswordMobile />} />
-      <Route path="/about" element={<AboutComputer />} />
-      <Route path="/howtoplay" element={<HowToPlayComputer />} />
-      <Route path="/contactus" element={<ContactUsComputer />} />
-      <Route path="/indextablet" element={<IndexTablet />} />
-      <Route path="/indexmobile" element={<IndexMobile />} />
-      <Route path="/abouttablet" element={<AboutTablet />} />
-      <Route path="/aboutmobile" element={<AboutMobile />} />
-      <Route path="/howtoplaytablet" element={<HowToPlayTablet />} />
-      <Route path="/howtoplaymobile" element={<HowToPlayMobile />} />
-      <Route path="/contactustablet" element={<ContactUsTablet />} />
-      <Route path="/contactusmobile" element={<ContactUsMobile />} />
-      <Route path="/friendsearch" element={<FriendSearchComputer />} />
-      <Route path="/landingtablet" element={<LandingTablet />} />
-      <Route path="/landingmobile" element={<LandingMobile />} />
-      <Route path="/profiletablet" element={<ProfileTablet />} />
-      <Route path="/profilemobile" element={<ProfileMobile />} />
-      <Route path="/usersearchtablet" element={<UserSearchTablet />} />
-      <Route path="/usersearchmobile" element={<UserSearchMobile />} />
-      <Route path="/friendsearchtablet" element={<FriendSearchTablet />} />
-      <Route path="/friendsearchmobile" element={<FriendSearchMobile />} />
+      <Route path="/index" element={<Index />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/howtoplay" element={<HowToPlay />} />
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="/resetpassword" element={<ResetPassword />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/usersearch" element={<UserSearch />} />
+      <Route path="/friendsearch" element={<FriendSearch />} />
+      <Route path="/game" element={<Game />} />
     </Routes>
   );
 }
