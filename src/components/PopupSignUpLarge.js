900566e0bc20dc3afe724fd8a14205ca8e1bd55a
Kevin <55450760+kevinbopp@users.noreply.github.com>
2023-07-31 00:06:06 -0400

    Update PopupSignUpLarge.js

--- src/components/PopupSignUpLarge.js
+++ src/components/PopupSignUpLarge.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 import PopupLoginLarge from "./PopupLoginLarge";
 import PortalPopup from "./PortalPopup";
 import { useNavigate } from "react-router-dom";
@@ -27,8 +27,6 @@ const PopupSignUpLarge = ({ onClose }) => {
     signUp();
   }, []);
 
-
-
   // Create a ref for each input element
   const inputUserRef = useRef(null);
   const inputEmailRef = useRef(null);
@@ -51,8 +49,41 @@ const PopupSignUpLarge = ({ onClose }) => {
     console.log("Password is " + password);
     console.log("confirmPassword is " + confirmPassword);
 
-    errorsRef.current.textContent = "Testing, 1 2 3...";
+    // Perform all validation/error messages.
+
+    errorsRef.current.textContent = "Check your email!";
+
+    // Prevent re-registering
   }, []);
+
+  // Function that sends a test email.
+  sendEmail(() =>
+  {
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: email, // recipient
+      from: 'kevinjunk3332@gmail.com', // All emails sent from my junk email
+      subject: 'Chess - Verify your email address',
+      text: 'Click below to verify!',
+      html: '<strong>To verify your email, click the following link:</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+      console.log('Email sent!')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }, []);
+
+    // Runs automatically when the component or page finishes loading.
+    useEffect(() => {
+      console.log('Page fully loaded!');
+  
+      errorsRef.current.textContent = "";
+    }, []);
 
   return (
     <>
