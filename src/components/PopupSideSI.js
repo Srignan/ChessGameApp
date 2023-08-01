12edebda6ac0b485d17eca7bf11e3ef80c3fc3d8
Kevin <55450760+kevinbopp@users.noreply.github.com>
2023-08-01 05:18:49 -0400

    Update PopupSideSI.js

--- src/components/PopupSideSI.js
+++ src/components/PopupSideSI.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./PopupSideSI.module.css";
 const PopupSideSI = ({ onClose }) => {
@@ -34,6 +34,7 @@ const PopupSideSI = ({ onClose }) => {
   }, []);
 
   const onDropLogOutTextClick = useCallback(() => {
+    logout();
     navigate("/");
   }, [navigate]);
 
@@ -57,6 +58,66 @@ const PopupSideSI = ({ onClose }) => {
     navigate("/friendsearch");
   }, [navigate]);
 
+    // Function to read the username and rating from the cookie
+  function readCookie() {
+  // Get the cookie string
+  const cookieString = document.cookie;
+
+  // Extract the value of the 'userData' cookie
+  const cookieName = 'userData=';
+  const cookieStartIndex = cookieString.indexOf(cookieName);
+  if (cookieStartIndex === -1) {
+    return null; // Cookie not found
+  }
+
+  let cookieValue = '';
+  const cookieEndIndex = cookieString.indexOf(';', cookieStartIndex);
+  if (cookieEndIndex === -1) {
+    cookieValue = cookieString.substring(cookieStartIndex + cookieName.length);
+  } else {
+    cookieValue = cookieString.substring(cookieStartIndex + cookieName.length, cookieEndIndex);
+  }
+
+  // Parse the JSON string to get the cookie data object
+  try {
+    const cookieData = JSON.parse(cookieValue);
+    return cookieData;
+  } catch (error) {
+    console.error('Error parsing cookie:', error);
+    return null;
+  }
+}
+
+// Ref for the welcome messages.
+  const popRef = useRef("");
+
+// Function to clear the cookie and navigate to the homepage
+function logout() {
+  // Clear the cookie by setting an expired date in the past
+  const cookieOptions = `expires=${new Date(0).toUTCString()}`;
+  document.cookie = `userData=; ${cookieOptions}`;
+}
+
+useEffect(() => {
+
+    // Function to read the cookie as soon as the page loads
+    const cookieData = readCookie();
+    if (cookieData) {
+      // Extract username and rating from the cookie data
+      const { username, rating } = cookieData;
+      console.log("Cookie read successful. Username is " + username + " and rating is " + rating + ".");
+      // Now you have the username and rating in separate variables,
+      // you can use them as needed in your component or application logic.
+
+      // Update welcome message
+      popRef.current.textContent = "Welcome, " + username + "!";
+    } else {
+      // Cookie not found or error reading it
+      console.log("Cookie read unsuccessful.");
+      // Handle as appropriate for your application
+    }
+  }, []);
+
   return (
     <div className={styles.popupsidesi} data-animate-on-scroll>
       <div className={styles.closespacer} />
@@ -67,7 +128,7 @@ const PopupSideSI = ({ onClose }) => {
         onClick={onClose}
       />
       <div className={styles.welcome}>
-        <b className={styles.welcomeheader}>Welcome, Username!</b>
+        <b ref={popRef} className={styles.welcomeheader}>Welcome, Username!</b>
         <b className={styles.welcomeusername}>Username!</b>
       </div>
       <div className={styles.sibuttons}>
